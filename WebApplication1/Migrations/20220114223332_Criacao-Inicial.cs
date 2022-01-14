94fbe492b64a9b6b1e01601574c67da759266724
unknown <carloseduuu333@gmail.com>
2022-01-14 20:51:23 -0300

    Api

---
+++ WebApplication1/Migrations/20220114223332_Criacao-Inicial.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace WebApplication1.Migrations
+{
+    public partial class CriacaoInicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Produto",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    CodigoProduto = table.Column<int>(type: "integer", nullable: false),
+                    Contexto = table.Column<string>(type: "text", nullable: true),
+                    UnidadeDePreco = table.Column<decimal>(type: "numeric", nullable: false),
+                    listaDePreco = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Produto", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Produto");
+        }
+    }
+}
---
+++ WebApplication1/Models/Produto.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApplication1.Models
+{
+    [Table("Produto")]
+    public class Produto
+    {
+        [Display(Name = "Codigo")]
+        [Column("Id")]
+        
+        public int id { get; set; }
+        [Display(Name = "CodigoProduto")]
+        [Column("CodigoProduto")]
+        public int ProductCode { get; set; }
+        
+        [Display(Name = "Contexto")]
+        [Column("Contexto")]
+        public string? Context { get; set; }
+        
+        [Display(Name = "UnidadeDePreco")]
+        [Column("UnidadeDePreco")]
+        public decimal UnitPrice { get; set; }
+        
+        [Display(Name = "listaDePreco")]
+        [Column("listaDePreco")]
+        public string? PriceList { get; set; }
+    }
+}
